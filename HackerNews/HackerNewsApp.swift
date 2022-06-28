76d98886cd1574a5360c6f43e43598a0ef6305f2
Abhiraj <abhirajbhattashali@gmail.com>
2022-06-28 12:24:05 +0530

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Abhiraj on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
